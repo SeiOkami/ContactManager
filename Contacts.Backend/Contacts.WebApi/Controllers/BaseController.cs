2a43cca34e6303622bfe0afbd680266826561c57
Vitaliy Chernenko <SeiOkami@gmail.com>
2023-04-12 08:46:13 +0300

    Delete test code

--- Contacts.Backend/Contacts.WebApi/Controllers/BaseController.cs
+++ Contacts.Backend/Contacts.WebApi/Controllers/BaseController.cs
@@ -16,16 +16,10 @@ namespace Contacts.WebApi.Controllers
         protected IMediator Mediator =>
             _mediator ??= HttpContext.RequestServices.GetService<IMediator>() ?? null!;
 
-        private readonly bool _testUser = false;
-        private readonly Guid _testUserID = Guid.Parse("20480835-FAA6-4495-8A7C-29E7CE175888");
-
         internal Guid UserId
         {
             get
             {
-                if (_testUser)
-                    return _testUserID;
-
                 var result = Guid.Empty;
 
                 if (User != null && User.Identity != null 
